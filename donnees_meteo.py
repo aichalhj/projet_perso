ebd6f6e73575ffcb21cc529441c97fcc34ee09f7
aichalhj <aicha.lahjiouj@etu.umontpellier.fr>
2023-10-11 13:20:00 +0200

    msj

--- donnees_meteo.py
+++ donnees_meteo.py
@@ -1,2 +1,21 @@
-#Url de l'API:https://api.open-meteo.com/v1/meteofrance?latitude=43.6109&longitude=3.8763&current=temperature_2m,relativehumidity_2m,apparent_temperature,is_day,precipitation,rain,snowfall&hourly=temperature_2m,apparent_temperature,precipitation,rain,snowfall&daily=apparent_temperature_max,apparent_temperature_min,sunrise,sunset,windspeed_10m_max,windgusts_10m_max&timezone=Europe%2FBerlin&past_days=5
+#Url de l'API:https://api.open-meteo.com/v1/forecast?latitude=43.6109&longitude=3.8763&current=temperature_2m,precipitation,rain,windspeed_10m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&past_days=5
 #Cle API : ac7e5560a7b3ec4021bfb9592b708c0e
+
+import requests
+
+api_key=f'ac7e5560a7b3ec4021bfb9592b708c0e'
+city:'Montpellier'
+
+#URL de la requête 
+url= f'https://api.open-meteo.com/v1/forecast?latitude=43.6109&longitude=3.8763&current=temperature_2m,precipitation,rain,windspeed_10m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&past_days=5'
+
+
+headers = { 'Authorization': f'Bearer {api_key}'}
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()
+    #Les données metéorologiques vont être stokées dans data
+    print(data)
+else:
+    print("Erreur lors de la requête: {response.status_code}")
