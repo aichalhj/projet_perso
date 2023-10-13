320ed1cca384198f6c93734336951bb90eae631f
aichalhj <aicha.lahjiouj@etu.umontpellier.fr>
2023-10-13 10:52:37 +0200

    msj

--- code_projet_perso.py
+++ code_projet_perso.py
@@ -1,7 +1,8 @@
-#Url de l'API:https://api.open-meteo.com/v1/meteofrance?latitude=43.6109&longitude=3.8763&current=temperature_2m,relativehumidity_2m,apparent_temperature,is_day,precipitation,rain,snowfall&hourly=temperature_2m,apparent_temperature,precipitation,rain,snowfall&daily=apparent_temperature_max,apparent_temperature_min,sunrise,sunset,windspeed_10m_max,windgusts_10m_max&timezone=Europe%2FBerlin&past_days=5
-#Cle API : ac7e5560a7b3ec4021bfb9592b708c0e
-
-
-
-import tkinter as tk
+import pytest
+import requests
 import json
+ 
+
+
+
+
