065e963fb982bf4ba475bac455cfb07f563943aa
aichalhj <aicha.lahjiouj@etu.umontpellier.fr>
2023-10-16 09:25:36 +0200

    msj

--- donnees_meteo.py
+++ donnees_meteo.py
@@ -1,10 +1,9 @@
-#Url de l'API:https://api.open-meteo.com/v1/forecast?latitude=43.6109&longitude=3.8763&current=temperature_2m,precipitation,rain,windspeed_10m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&past_days=5
 #Cle API : ac7e5560a7b3ec4021bfb9592b708c0e
 
 import requests
 
 api_key=f'ac7e5560a7b3ec4021bfb9592b708c0e'
-city:'Montpellier'
+city=Montpellier
 
 #URL de la requête 
 url= f'https://api.open-meteo.com/v1/forecast?latitude=43.6109&longitude=3.8763&current=temperature_2m,precipitation,rain,windspeed_10m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&past_days=5'
