acc7ae457aa4c21d561f189861f39f178e6e0be2
aichalhj <aicha.lahjiouj@etu.umontpellier.fr>
2023-10-30 13:40:49 +0100

    ajout données méteo

--- donnees_meteo.py
+++ donnees_meteo.py
@@ -3,72 +3,62 @@ import json
 from PIL import Image
 import pandas as pd
 import datetime
+from IPython.display import Markdown
 
-def get_weather_data():
-    #Obtenez la date actuelle.
-    date_actuelle = datetime.date.today()
 
-    # Calculez la date de fin (5 jours à partir de la date actuelle).
-    date_fin = date_actuelle + datetime.timedelta(days=5)
+#Obtenez la date actuelle.
+date_actuelle = datetime.date.today()
 
-    # Formattez les dates au format AAAA-MM-JJ.
-    date_debut_str = date_actuelle.strftime('%Y-%m-%d')
-    date_fin_str = date_fin.strftime('%Y-%m-%d')
+# Calculez la date de fin (5 jours à partir de la date actuelle).
+date_fin = date_actuelle + datetime.timedelta(days=5)
 
-    # Construisez l'URL avec les dates mises à jour.
-    base_url = "https://api.open-meteo.com/v1/meteofrance?latitude=43.6109&longitude=3.8763&current=temperature_2m,precipitation,rain&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,windspeed_10m_max&timezone=Europe%2FBerlin"
-    meteo_url = f"{base_url}&start_date={date_debut_str}&end_date={date_fin_str}"
+# Formattez les dates au format AAAA-MM-JJ.
+date_debut_str = date_actuelle.strftime('%Y-%m-%d')
+date_fin_str = date_fin.strftime('%Y-%m-%d')
 
-    response = requests.get(meteo_url)
+# Construisez l'URL avec les dates mises à jour.
+base_url = "https://api.open-meteo.com/v1/meteofrance?latitude=43.6109&longitude=3.8763&current=temperature_2m,precipitation,rain,weathercode&hourly=temperature_2m,relativehumidity_2m,rain,snowfall,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,windspeed_10m_max&timezone=Europe%2FBerlin&start_date=2023-10-30&end_date=2023-11-03"
+meteo_url = f"{base_url}&start_date={date_debut_str}&end_date={date_fin_str}"
 
-    if response.status_code == 200:
-        data = response.json()
-        # Traitement des données ici
-        print("Données météo mises à jour avec succès.",data)
-        #Création Dataframe
-        df_data=pd.DataFrame(data)
-        pd.set_option("display.max_rows",None)
-        #Création d'un CSV associé
-        df_data.to_csv("df_data.csv",index=False)
-        #Extraction des données
-        tempmax=data["daily"]["temperature_2m_max"]
-        tempmin=data["daily"]["temperature_2m_min"]
-        sunrice=data ["daily"]["sunrise"]
-        windspeed=data ["daily"]["windspeed_10m_max"]
-        sunset=data ["daily"]["sunset"]
-        weather_code = data.get("weather_code", [])
-        print("weather_code",weather_code)
+response = requests.get(meteo_url)
+
+if response.status_code == 200:
+    data = response.json()
+    # Traitement des données ici
+    print("Données météo mises à jour avec succès.",data)
+    #Création Dataframe
+    df_data=pd.DataFrame(data)
+    pd.set_option("display.max_rows",None)
+    #Création d'un CSV associé
+    df_data.to_csv("df_data.csv",index=False)
+    #Extraction des données
+    temp = data[0]["hourly"]["temperature_2m"]
+    windspeed = data[0]["daily"]["windspeed_10m_max"]
+    sunset = data[0]["daily"]["sunset"]
+    #rain = data[0]["daily"]["precipitation"]
+    sunrise = data[0]["daily"]["sunrise"]
+    humidity = data[0]["hourly"]["relativehumidity_2m"]
+
+    
+print("la taille est de ",len(temp))
+#rain = data ["daily"]["precipitation"]
+
+'''min_length = min(len(tempmax), len(tempmin), len(windspeed), len(sunrise), len(sunset))
+    tempmax = tempmax[:min_length]
+    tempmin = tempmin[:min_length]
+    windspeed = windspeed[:min_length]
+    sunset = sunset[:min_length]
+    #rain=rain[:min_length]
+    sunrise=sunrise[:min_length]
         
-    else:
-        print("Échec de la requête météo. Code d'état :", response.status_code) 
-        # Supposons que 'data' contienne les données météo avec la condition météorologique
-        condition_meteo = data.get("weather_condition", "Inconnu")
+    df = pd.DataFrame ({
+    "Date": [date_debut_str]*min_length,
+    "Température Maximale (°C)": tempmax,
+    "Température Minimale (°C)": tempmin,
+    "Lever du Soleil": sunrise,
+    "Vitesse du Vent Maximale (km/h)": windspeed,
+    "Coucher du Soleil": sunset 
+    #"Précipitation (mm)": rain 
+    })
+    print(df)'''
 
-        # Associez la condition météorologique à l'image correspondante
-        image_path = get_image_for_weather_condition(condition_meteo)
-
-        if image_path:
-            print(f'Chemin de l\'image météo : {image_path}')
-            # Affichez l'image ici avec Pillow
-            image = Image.open(image_path)
-            image.show()
-        else:
-            print('Image météo non trouvée pour la condition météorologique.')
-
-
-def get_image_for_weather_condition(condition):
-    # Associez les conditions météorologiques aux chemins d'images correspondants
-    conditions_images = {
-        "Ensoleillé": "projet_perso/image_meteo/sun.svg",
-        "Nuageux": "projet_perso/image_meteo/couvert.svg",
-        "Pluvieux": "projet_perso/image_meteo/rain.svg",
-        "Orage": "thunderstorm.png",
-        "Vent": "projet_perso/image_meteo/wind.svg",
-        "Inconnu": "unknown.png"  # Image pour les conditions inconnues
-    }
-
-    # Récupérez le chemin de l'image en fonction de la condition météorologique
-    return conditions_images.get(condition, None)
-
-get_weather_data()
-get_image_for_weather_condition()
